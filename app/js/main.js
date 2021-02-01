1ee0b935e3eff87451f940028bdc280a9cc23da4
Polina Sinitsyna <Pasi6@yandex.ru>
2021-02-01 22:00:16 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
 
 $(document).ready(function(){
-  $('.team-card-container').slick();
+  $('.slider-testimonials').slick({
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 });
+
+$(document).ready(function(){
+  $('.slider-team').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: false,
+    // autoplay: true,
+    // autoplaySpeed: 2000
+  });
+});
+
+
