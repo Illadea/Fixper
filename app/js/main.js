5950f654ecef22c02d55f0d17abdcc220ba6dbc1
Polina Sinitsyna <Pasi6@yandex.ru>
2021-02-02 20:57:13 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -16,9 +16,37 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     infinite: false,
-    // autoplay: true,
-    // autoplaySpeed: 2000
+    autoplay: true,
+    autoplaySpeed: 2000
   });
 });
 
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+              center: [55.766238, 37.605726],
+              zoom: 13
+        });
+    // Создаем геообъект
+    var geoObjectOffice = new ymaps.Placemark([55.765954, 37.565147], {
+            iconCaption: 'Fixper (офис)',
+            balloonContent: '<strong>Москва, Расторгуевский переулок, 16с1</strong> <br>8:00—21:00 без выходных'
+            }, {
+            preset: 'islands#dotIcon',
+            iconColor: '#96317D'
+        });
+        var geoObjectCallcenter = new ymaps.Placemark([55.774715, 37.625154], {
+          iconCaption: 'Fixper (диспетчерская)',
+          balloonContent: '<strong>Москва, Троицкая улица, 10с1</strong> <br>7:00—23:00 без выходных'
+          }, {
+          preset: 'islands#dotIcon',
+          iconColor: '#96317D'
+      });
+
+  // Размещение геообъекта на карте.
+  myMap.geoObjects.add(geoObjectOffice)
+                  .add(geoObjectCallcenter);
+}
 
